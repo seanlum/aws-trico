ceb3b2235d4ff46c603a204111af109591d7f02a
seanlum <seanjklum@gmail.com>
2024-06-03 15:29:57 -0700

    [main] Removed old json import to amplify_outputs.json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify'
-import config from './amplify_outputs.json'
 import { ThemeProvider } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import App from './App'
